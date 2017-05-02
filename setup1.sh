3ff5328eb3568b5d92687f9a8187afaae2ee402d
Kavya <gkavya10@gmail.com>
2017-05-02 11:19:05 +0530

    clone github repos

---
+++ setup1.sh
@@ -0,0 +1,10 @@
+# To setup system based on my configurations
+
+# Step 1: clone dotfiles and dwm configurations
+
+cd
+mkdir customization
+cd customization
+
+git clone https://github.com/SilverSoldier/dotfiles.git 
+git clone https://github.com/SilverSoldier/my-dwm.git
